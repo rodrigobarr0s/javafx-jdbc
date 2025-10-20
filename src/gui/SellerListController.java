ecc6ed7fd2004a9076cc86589fc1f517b5096298
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-10-20 06:59:30 -0300

    Seller TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -41,6 +42,15 @@ public class SellerListController implements Initializable, DataChangeListener {
     private TableColumn<Seller, String> tableColumnName;
 
     @FXML
+    private TableColumn<Seller, String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<Seller, Date> tableColumnBirthDate;
+
+    @FXML
+    private TableColumn<Seller, Double> tableColumnBaseSalary;
+
+    @FXML
     private TableColumn<Seller, Seller> tableColumnEDIT;
 
     @FXML
@@ -68,6 +78,11 @@ public class SellerListController implements Initializable, DataChangeListener {
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyy");
+        tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+        Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
@@ -85,24 +100,24 @@ public class SellerListController implements Initializable, DataChangeListener {
 
     private void createDialogForm(Seller obj, String absolutePath, Stage parentStage) {
         // try {
-        //     FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutePath));
-        //     Pane pane = loader.load();
+        // FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutePath));
+        // Pane pane = loader.load();
 
-        //     SellerFormController controller = loader.getController();
-        //     controller.setSeller(obj);
-        //     controller.setService(new SellerService());
-        //     controller.subscribeDataChangeListener(this);
-        //     controller.updateFormData();
+        // SellerFormController controller = loader.getController();
+        // controller.setSeller(obj);
+        // controller.setService(new SellerService());
+        // controller.subscribeDataChangeListener(this);
+        // controller.updateFormData();
 
-        //     Stage dialogStage = new Stage();
-        //     dialogStage.setTitle("Enter Seller data");
-        //     dialogStage.setScene(new Scene(pane));
-        //     dialogStage.setResizable(false);
-        //     dialogStage.initOwner(parentStage);
-        //     dialogStage.initModality(Modality.WINDOW_MODAL);
-        //     dialogStage.showAndWait();
+        // Stage dialogStage = new Stage();
+        // dialogStage.setTitle("Enter Seller data");
+        // dialogStage.setScene(new Scene(pane));
+        // dialogStage.setResizable(false);
+        // dialogStage.initOwner(parentStage);
+        // dialogStage.initModality(Modality.WINDOW_MODAL);
+        // dialogStage.showAndWait();
         // } catch (IOException e) {
-        //     Alerts.showError("IO Exception", "Error loading view", e.getMessage());
+        // Alerts.showError("IO Exception", "Error loading view", e.getMessage());
         // }
     }
 
